6b6d21831facd203709a3fdc42cc521b8c09f22a
Freddie Duffield <fwhduffield@gmail.com>
2019-08-13 12:03:03 +0100

    up to first excercise- finished lesson 23

---
+++ udemy-advanced-react-testing/src/components/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {shallow} from 'enzyme'        
+
+import App from '../App'
+import CommentBox from '../CommentBox'
+import CommentList from '../CommentList'
+
+
+it('shows a comment box', () => {
+    const wrapped = shallow(<App />)
+    expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('shows a comment list', () => {
+    const wrapped = shallow(<App />)
+    expect(wrapped.find(CommentList).length).toEqual(1)
+})

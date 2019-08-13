d630257d4fa42c088e8a17a129f99f314bd3dbcd
Freddie Duffield <fwhduffield@gmail.com>
2019-08-13 10:57:07 +0100

    finishing testing lessons

--- test-react-components-with-enzyme-jest/src/App.js
+++ test-react-components-with-enzyme-jest/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Test = () => <div>Testing</div>;
+// const Test = () => <div>Testing</div>;
 const Title = ({ text }) => <div>{text}</div>;
 
 class App extends Component {

aa958d6bebf0b7c2d2de5f15e0b366dd7feb7fa8
Freddie Duffield <fwhduffield@gmail.com>
2019-09-25 16:15:14 +0100

    first 2 lessons of javascript promises indepth

---
+++ javascript-promises-indepth/lesson-2/index.js
@@ -0,0 +1,29 @@
+const API_URL = "https://starwars.egghead.training/";
+const output = document.getElementById("output");
+
+output.innerText = "Loading... "
+ 
+fetch(API_URL + "films")
+  .then(
+    response => {
+      if(!response.ok) {
+        throw Error("Unsuccessful response")
+      }
+
+      return response.json().then(films => { 
+        output.innerText = getFilmTitles(films);
+      })
+      .catch(error => {
+        console.warn(error);
+        output.innerText = ":(";
+      })
+    }
+  )
+ 
+
+  function getFilmTitles(films) {
+    return films
+      .sort((a,b) => a.episode_id - b.episode_id)
+      .map(film => `${film.episode_id}. ${film.title}`)
+      .join("\n")
+  }
